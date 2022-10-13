a9c65d61988d14c149453d552c421bed7a6b0037
kostya-over <kostaover940@gmail.com>
2022-10-13 10:31:59 +0300

    Created initial configuration and logging.

---
+++ CompanyEmployees.Api/Extentions/ServiceExtentions.cs
@@ -0,0 +1,25 @@
+﻿using Contracts;
+using LoggerService;
+
+namespace CompanyEmployees.Api.Extentions;
+
+public static class ServiceExtentions
+{
+    public static void ConfigureCors(this IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+            });
+        });
+    }
+
+    public static void ConfigureLoggerService(this IServiceCollection services)
+    {
+        services.AddSingleton<ILoggerManager, LoggerManager>();
+    }
+}
---
+++ CompanyEmployees.Api/Program.cs
@@ -0,0 +1,34 @@
+using CompanyEmployees.Api.Extentions;
+using NLog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+//Add NLog configuration
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.ConfigureCors();
+builder.Services.ConfigureLoggerService();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
